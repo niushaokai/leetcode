121344aa53d62531256d5539a32c45c50bdce449
niushaokai <niushaokai@shuidihuzhu.com>
2020-04-06 16:02:02 +0800

    add 6

--- src/medium/Convert6.java
+++ src/medium/Convert6.java
@@ -1,28 +1,60 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author niushaokai
  * @date 2020/4/1
  */
 public class Convert6 {
-    public String convert(String s, int nRows) {
-        if (s == null || s.length() == 0 || nRows <= 0)
-            return "";
-        if (nRows == 1)
-            return s;
-
-        StringBuilder res = new StringBuilder();
-        int size = 2 * nRows - 2;
-        for (int i = 0; i < nRows; i++) {
-            for (int j = i; j < s.length(); j += size) {
-                res.append(s.charAt(j));
-                if (i != 0 && i != nRows - 1) {//except the first row and the last row
-                    int temp = j + size - 2 * i;
-                    if (temp < s.length())
-                        res.append(s.charAt(temp));
-                }
-            }
+    public String convert(String s, int numRows) {
+        if (numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            //第一行和最后一行该拐了
+            //在第一行的时候flag=1，代表每次获取rows中的下一个StringBuilder
+            //在最后一行的时候flag=-1，代表每次获取rows中的上一个StringBuilder
+            if (i == 0 || i == numRows - 1) flag = -flag;
+            i += flag;
         }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) res.append(row);
         return res.toString();
     }
+
+
+    //自己写的
+    public String convert2(String s, int numRows) {
+        if (numRows < 2)
+            return s;
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            list.add(new StringBuilder());
+        }
+
+        int row = 0, flag = -1;
+        for (char c : s.toCharArray()) {
+            if (row == 0 || row == numRows - 1)
+                flag = -flag;
+            list.get(row).append(c);
+            row += flag;
+        }
+
+        String result = "";
+        for (StringBuilder sb : list) {
+            result += sb.toString();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Convert6 convert6 = new Convert6();
+        String s = "LEETCODEISHIRING";
+        System.out.println(convert6.convert2(s, 3));
+    }
+
 }
